10aa4dcfa6da355e47263f2757cb744a2eac1a58
milan-bavishi <milanbavishi99@gmail.com>
2025-04-03 18:32:10 +0530

    Update project

---
+++ studynotion-backend/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+
+const contactusRoutes = require("./routes/Contactus")
+const userRoutes = require("./routes/User");
+const paymentRoutes = require("./routes/Payments");
+const profileRoutes = require("./routes/Profile");
+const courseRoutes = require("./routes/Course");
+
+const Database = require("./config/Database");
+const cookieParser = require("cookie-parser");
+
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const {cloudinaryconnect} = require("./config/Cloudinary")
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = 5000;
+Database.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+cloudinaryconnect();
+
+app.use("/api/auth",userRoutes);
+app.use("/api/contact",contactusRoutes);
+app.use("/api/payment",paymentRoutes);
+app.use("/api/course",courseRoutes);  
+app.use("/api/profile",profileRoutes);
+
+  app.get("/api/welcome", (req, res) => {
+    res.status(200).json({
+      message: "Welcome to API ",
+    });
+  });
+
+  app.get("/api/testing", (req,res) => {
+    res.status(200).json({
+      message: "Welcome to the Testing Endpoint ",
+    });
+  });
+
+  
+  app.use("/api/products", (req, res) => {
+    return res.status(200).json({
+      message: 'This is new feature change, a new route Milan'
+    })
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
